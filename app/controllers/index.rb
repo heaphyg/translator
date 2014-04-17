e98816ea166826a847a3cd8ab12d2f4fb37b3f76
Greg Heaphy <heaphyg@gmail.com>
2014-04-17 17:25:49 -0500

    implemented error message in translation view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,10 +5,18 @@ end
 post '/' do
   if params[:language] == "english"
     @english_word = EnglishWord.find_by_word(params[:word])
-    @german_words = @english_word.german_words
+    if @english_word.nil?
+    	@error = {invalid: "#{params[:word]} is not in the database"}
+    else
+      @german_words = @english_word.german_words
+    end
   else
     @german_word = GermanWord.find_by_word(params[:word])
-    @english_words = @german_word.english_words
+    if @german_word.nil?
+      @error = {invalid: "#{params[:word]} is not in the database"}
+    else
+      @english_words = @german_word.english_words
+    end
   end
   erb :index
 end
