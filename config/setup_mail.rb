3d435cd72ea0d37f75c51fca8bb0a293a1a17abb
Harry Wang <hgw2101@gmail.com>
2014-04-18 14:13:31 -0500

    finish actionmailer for signup and for creating a new word

--- config/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address                => "smtp.gmail.com",
-  :port                   => "587",
-  :domain                 => "harrywang7.com",
-  :user_name              => "baddass.translator",
-  :password               => "germaniscool151",
-  :authentication         => "plain",
-  :enable_starttls_auto   => true
-}
